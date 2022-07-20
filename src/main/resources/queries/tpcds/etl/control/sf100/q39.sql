ae5bfee97e9bf9afdaff62eaaceb698d253d2e92
Andrii Rosa <andriyrosa@gmail.com>
2022-07-20 19:34:52 -0400

    Fix flaky tpcds/etl/q39 query
    Covarience introduces variability and the query becomes flaky for
    larger data sets (sf10000+)

--- src/main/resources/queries/tpcds/etl/control/sf100/q39.sql
+++ src/main/resources/queries/tpcds/etl/control/sf100/q39.sql
@@ -2,5 +2,5 @@ SELECT *
 FROM
 (
   VALUES
-    (X'e0 3a 3d cf 3b b7 8c 01', X'e3 c0 25 4f 36 f7 ff 59', X'60 95 20 5b 36 d2 c7 47', X'2a bc c6 5b 1f 18 71 f3', X'14 4e f8 19 eb 7b fe 8d', X'00 ca 55 e0 15 9a b0 7f', X'bb 32 2d 0d e3 70 68 37', X'ca f8 99 ba 1e 60 ee 05')
+    (X'e0 3a 3d cf 3b b7 8c 01', X'e3 c0 25 4f 36 f7 ff 59', X'60 95 20 5b 36 d2 c7 47', X'2a bc c6 5b 1f 18 71 f3', X'00 ca 55 e0 15 9a b0 7f', X'bb 32 2d 0d e3 70 68 37')
 )
