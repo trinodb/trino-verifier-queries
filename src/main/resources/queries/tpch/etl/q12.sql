3bece2d14dabe704d7964b3e7476f56cb16fcbdd
Andrii Rosa <andriyrosa@gmail.com>
2022-07-25 16:55:08 -0400

    Ensure same result independent of table column order

--- src/main/resources/queries/tpch/etl/q12.sql
+++ src/main/resources/queries/tpch/etl/q12.sql
@@ -1,7 +1,31 @@
 CREATE TABLE q12
 AS
 SELECT
-  *,
+  o_orderkey,
+  o_custkey,
+  o_orderstatus,
+  o_totalprice,
+  o_orderdate,
+  o_orderpriority,
+  o_clerk,
+  o_shippriority,
+  o_comment,
+  l_orderkey,
+  l_partkey,
+  l_suppkey,
+  l_linenumber,
+  l_quantity,
+  l_extendedprice,
+  l_discount,
+  l_tax,
+  l_returnflag,
+  l_linestatus,
+  l_shipdate,
+  l_commitdate,
+  l_receiptdate,
+  l_shipinstruct,
+  l_shipmode,
+  l_comment,
   CASE
       WHEN o_orderpriority = '1-URGENT'
            OR o_orderpriority = '2-HIGH'
--- src/main/resources/queries/tpch/etl/q4.sql
+++ src/main/resources/queries/tpch/etl/q4.sql
@@ -1,7 +1,15 @@
 CREATE TABLE q4
 AS
 SELECT
-  *
+  o_orderkey,
+  o_custkey,
+  o_orderstatus,
+  o_totalprice,
+  o_orderdate,
+  o_orderpriority,
+  o_clerk,
+  o_shippriority,
+  o_comment
 FROM orders
 WHERE
   o_orderdate >= DATE '1993-07-01'
